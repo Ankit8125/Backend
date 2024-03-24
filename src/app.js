137d2464ba95ff7590de02805b44977b8c428325
Ankit Verma <syncing284@gmail.com>
2024-03-24 15:49:32 +0530

    util code updation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// now we can get data in any form, so below is code to get data in those forms
+app.use(express.json({limit: "20kb"})) // means express can accept json
+app.use(express.urlencoded({extended: true, limit:"20kb"}))
+app.use(express.static("public"))// public folder: we keep assets/images/ anything
+app.use(cookieParser())
 
 export {app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)) 
+        .catch((err)=> next(err))
+        // .resolve -> execute func. | .catch ~ reject
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) = () => {}
+// const asyncHandler = (func) = async() => {}
+
+// doing via try-catch, above one is doing via promises
+
+// const asyncHandler = (fn) => async(req,res,next) =>{
+//     try {
+//         await fn(req,res,next)        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
